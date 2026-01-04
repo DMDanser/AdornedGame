550d9e4e2656c991ba5d04c18343aa6b67624464
DMDanser <155984953+DMDanser@users.noreply.github.com>
2026-01-04 12:09:13 -0500

    Initial push

---
+++ Adorned/Source/Adorned/AdornedGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdornedGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class AAdornedGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAdornedGameMode();
+};
+
+
+
