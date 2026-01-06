40b521318b5778a4eb65d575d75141885f692bcf
DMDanser <155984953+DMDanser@users.noreply.github.com>
2026-01-06 12:03:59 -0500

    Apply .gitignore retroactively.

--- Adorned/Source/Adorned/Adorned.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Adorned.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Adorned, "Adorned" );
-
-DEFINE_LOG_CATEGORY(LogAdorned)
--- Adorned/Source/Adorned/AdornedGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AdornedGameMode.h"
-
-AAdornedGameMode::AAdornedGameMode()
-{
-	// stub
-}
